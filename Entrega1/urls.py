dc3c90956b0284af9a0e73768d1e66840ac29eb6
Martin Goñi <mar7in.goni@gmail.com>
2022-08-01 22:48:06 -0300

    Emprolije listas y botones

--- Entrega1/urls.py
+++ Entrega1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Appventas/', include("Appventas.urls")),
+   # path('', inicio, name="inicio")
    
 ]
