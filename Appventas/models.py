1430dac90a788a601aed6c644e170d004a1899eb
Martin Goñi <mar7in.goni@gmail.com>
2022-08-11 23:54:10 -0300

    Agregue los formularios de empleados y cliente. Hice los modelos para heredar de productos y perfiles

--- Appventas/models.py
+++ Appventas/models.py
@@ -1,44 +1,63 @@
+
 from django.db import models
 
 # Create your models here.
 from django.db import models
-idB=0
 
-# Create your models here.
-class bicicletas(models.Model):
-    id=models.BigAutoField(primary_key=True)    
+
+
+class producto (models.Model):
+   #id=models.BigAutoField(primary_key=True)
+    precio = models.IntegerField()
     marca = models.CharField(max_length=30)
     modelo = models.CharField(max_length=30)
+   
+
+    class Meta:
+        abstract = True
+
+# Create your models here.
+class bicicletas(producto):
     rodado = models.CharField(max_length=30)
     color = models.CharField(max_length=30)
-    precio = models.IntegerField()
-
+   
     def __str__(self):
-        return f"{self.id,self.marca, self.modelo,self.rodado,self.color,self.precio}"
+        return f"{self.marca, self.modelo,self.rodado,self.color,self.precio}"
 
 
-class repuestos(models.Model):
-    id=models.BigAutoField(primary_key=True)
+class repuestos(models.Model):#Borre fabricante
     tipo = models.CharField(max_length=30)
-    marca = models.CharField(max_length=30)
-    modelo = models.CharField(max_length=30)
-    fabricante = models.CharField(max_length=30)
-    precio = models.IntegerField()
-
+   
     def __str__(self):
-        return f"{self.id,self.tipo,self.marca, self.modelo,self.fabricante,self.precio}"
+        return f"{self.tipo,self.marca, self.modelo,self.precio}"
     
 
 class indumentaria(models.Model):
-    id=models.BigAutoField(primary_key=True)
     tipo = models.CharField(max_length=30)
-    marca = models.CharField(max_length=30)
-    modelo = models.CharField(max_length=30)
     talle = models.CharField(max_length=30)
-    precio = models.IntegerField()
+    
 
     def __str__(self):
-        return f"{self.id,self.tipo,self.marca, self.modelo,self.talle,self.precio}"
+        return f"{self.tipo,self.marca, self.modelo,self.talle,self.precio}"
+
+class perfiles(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    telefono= models.IntegerField() 
+    emial=models.EmailField(max_length=100)
+    class Meta:
+        abstract=True
+
+class empleado(perfiles):
+   cargo=models.CharField(max_length=30)
+   def __str__(self):
+    return f"{self.nombre,self.apellido, self.telefono,self.emial,self.cargo}"
+
+class cliente(perfiles):
+    edad=models.IntegerField()
+    def __str__(self):
+     return f"{self.nombre,self.apellido, self.telefono,self.emial,self.edad}"
+
 
 class EnviarMensajes(models.Model):
 
@@ -49,3 +68,5 @@ class EnviarMensajes(models.Model):
        
     def __str__(self):
         return f"{self.nombre,self.correo,self.telefono, self.mensaje}"
+
+#Comentar todo: selecciono . 1°ctrl+k . 2°ctrl+c (comentar) 2°ctrl+u (descomentar)
